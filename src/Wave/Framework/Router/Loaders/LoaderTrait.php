2ecc8403a533a77d5d0f887737b4cf20d9203120
DaGhostman Dimitrov <daghostman.dimitrov@gmail.com>
2015-02-04 21:11:41 +0000

    Added initial support for route loaders

---
+++ src/Wave/Framework/Router/Loaders/LoaderTrait.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Wave\Framework\Router\Loaders;
+
+
+trait LoaderTrait
+{
+    public function pushRoutes($router)
+    {
+        if (!method_exists($router, 'addRoute')) {
+            throw new \LogicException(
+                'Provided router should have public method \'addRoute\''
+            );
+        }
+
+        foreach ($this->raw as $route) {
+            var_dump($route);
+            $router->addRoute($route->method, $route->uri, $route->callback);
+        }
+    }
+
+    public function pushControllers($router)
+    {
+        if (!method_exists($router, 'controller')) {
+            throw new \LogicException(
+                'Provided router should have public method \'controller\''
+            );
+        }
+
+        foreach ($this->raw as $route) {
+            $router->controller($route['uri'], $route['class'], $route['filters'] ?: []);
+        }
+    }
+
+    public function pushFilters($router)
+    {
+        if (!method_exists($router, 'filter')) {
+            throw new \LogicException(
+                'Provided router should have public method \'addRoute\''
+            );
+        }
+
+        foreach ($this->raw as $filter) {
+            $router->filter($filter['name'], $filter['callback']);
+        }
+    }
+}
---
+++ src/Wave/Framework/Router/Loaders/XmlLoader.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: daghostman
+ * Date: 02/02/15
+ * Time: 23:18
+ */
+
+namespace Wave\Framework\Router\Loaders;
+
+class XmlLoader
+{
+    use LoaderTrait;
+
+    protected $raw = null;
+    public function __construct($xml)
+    {
+        $this->raw = new \SimpleXMLElement($xml, 0, is_file($xml));
+
+
+        if (!$this->raw) {
+            throw new \InvalidArgumentException(
+                'Unable to load XML'
+            );
+        }
+    }
+}
