6e0f4215d9d13e65669853479bb39a32aba83a1b
Dimitar Dimitrov <daghostman.dd@gmail.com>
2015-02-21 13:18:50 +0000

    PSR-7 implementation

--- src/Wave/Framework/Http/Request.php
+++ src/Wave/Framework/Http/Request.php
@@ -5,14 +5,17 @@
  * Date: 24/08/14
  * Time: 00:17
  */
-
 namespace Wave\Framework\Http;
+
+use Psr\Http\Message\RequestInterface;
 
 class Request
 {
 
     protected $request = [];
+
     protected $headers = [];
+
     protected $vars = [];
 
     protected $source = [];
@@ -21,43 +24,29 @@ class Request
 
     /**
      * Creates a request object and accepts an array with key => value
-     * pairs which in most cases of Web Apps should be $_SERVER. In scenario where
+     * pairs which in most cases of Web Apps should be $_SERVER.
+     * In scenario where
      * CLI functionality it will require the following keys: 'REQUEST_URI',
      * 'REQUEST_METHOD' and 'REQUEST_PROTOCOL'. The URI should be translated to
      * look like usual HTTP URI and method could be anything, only requirement
      * is that it should be the same as the one assigned to the Controller.
      *
-     * @param $source array Array source of information
-     * @param $requestData array Should contain a mix of variables to pass merged $_GET
-     *                           and $_POST, $_REQUEST, etc.
+     * @param $source array
+     *            Array source of information
+     * @param $requestData array
+     *            Should contain a mix of variables to pass merged $_GET
+     *            and $_POST, $_REQUEST, etc.
      */
     public function __construct($source, $requestData = [])
     {
         $this->source = $source; // For access with __get
-
+        
         $this->params = $requestData;
         foreach ($source as $name => $value) {
             if (substr($name, 0, 5) == 'HTTP_') {
-                $this->headers[str_replace(
-                    ' ',
-                    '',
-                    ucwords(
-                        strtolower(
-                            str_replace('_', ' ', substr($name, 5))
-                        )
-                    )
-                )] = $value;
-
+                $this->headers[str_replace(' ', '', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
             } elseif (substr($name, 0, 8) == 'REQUEST_') {
-                $this->request[str_replace(
-                    ' ',
-                    '',
-                    ucwords(
-                        strtolower(
-                            str_replace('_', ' ', substr($name, 8))
-                        )
-                    )
-                )] = $value;
+                $this->request[str_replace(' ', '', ucwords(strtolower(str_replace('_', ' ', substr($name, 8)))))] = $value;
             }
         }
     }
@@ -65,17 +54,23 @@ class Request
     /**
      * A getter for all entries which begin with 'REQUEST_' in the $_SERVER variable.
      *
-     * @param $name string The string to search for. Example 'UserAgent' for 'REQUEST_USER_AGENT'
-     *
+     * @param $name string
+     *            The string to search for. Example 'UserAgent' for 'REQUEST_USER_AGENT'
+     *            
      * @return mixed The value or null if the key is not found
      */
     public function request($name)
     {
-        if (!array_key_exists($name, $this->request)) {
+        if (! array_key_exists($name, $this->request)) {
             return null;
         }
-
+        
         return $this->request[$name];
+    }
+
+    public function __set($key, $value)
+    {
+        $this->params[$key] = $value;
     }
 
     /**
@@ -88,6 +83,11 @@ class Request
         return $this->request('Uri');
     }
 
+    public function getUri()
+    {
+        return $this->uri();
+    }
+
     /**
      * Alias of Request::request('Method');
      *
@@ -98,19 +98,25 @@ class Request
         return $this->request('Method');
     }
 
+    public function getMethod()
+    {
+        return $this->method();
+    }
+
     /**
      * Gives access to 'HTTP_' from $_SERVER
      *
-     * @param $name string Header name. Example 'AcceptEncoding'
-     *
+     * @param $name string
+     *            Header name. Example 'AcceptEncoding'
+     *            
      * @return null
      */
     public function header($name)
     {
-        if (!array_key_exists($name, $this->headers)) {
+        if (! array_key_exists($name, $this->headers)) {
             return null;
         }
-
+        
         return $this->headers[$name];
     }
 
@@ -120,19 +126,20 @@ class Request
     }
 
     /**
-     * Returns the requested parameter. Looks up in $_GET and $_POST. Note:
+     * Returns the requested parameter.
+     * Looks up in $_GET and $_POST. Note:
      * if there is a duplicate key an array is returned array(0 => GET, 1 => POST)
      *
-     * @param $name string
+     * @param $name string            
      *
      * @return mixed
      */
     public function param($name)
     {
-        if (!array_key_exists($name, $this->params)) {
+        if (! array_key_exists($name, $this->params)) {
             return null;
         }
-
+        
         return $this->params[$name];
     }
 
@@ -148,10 +155,10 @@ class Request
 
     public function __get($key)
     {
-        if (!array_key_exists(strtoupper($key), $this->source)) {
+        if (! array_key_exists(strtoupper($key), $this->source)) {
             return null;
         }
-
+        
         return $this->source[strtoupper($key)];
     }
 
@@ -163,4 +170,7 @@ class Request
             'params' => $this->params
         ];
     }
+
+    public function getRequestTarget()
+    {}
 }
