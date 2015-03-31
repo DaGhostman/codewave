f75a90be63c93908f99988e6ed4997ed181163e6
DaGhostman Dimitrov <daghostman.dimitrov@gmail.com>
2015-03-31 17:55:32 +0100

    Added doc blocks and removed redundant method `update`

--- src/Wave/Framework/Adapters/Link/Linkable.php
+++ src/Wave/Framework/Adapters/Link/Linkable.php
@@ -12,9 +12,26 @@ use \Wave\Framework\Common\Link;
 
 interface Linkable
 {
+    /**
+     * Performs a transparent update on all links, only the Destination objects
+     * should be aware of the linkages (although it is optional, it is recommended).
+     *
+     * @return mixed Triggers the Link::notify so that the destinations are
+     * updated and perform the necessary actions.
+     */
     public function notify();
-    public function addLink(Link $link);
-    public function update();
 
+    /**
+     * Pushes the link in the object, so that the object can independently
+     * trigger the update process.
+     *
+     * @param \Wave\Framework\Common\Link $link
+     * @return mixed
+     */
+    public function addLink(Link $link);
+
+    /**
+     * @return mixed Cloned instance of the object which is being transparent
+     */
     public function getState();
 }
