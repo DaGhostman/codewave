fac346f3c605ec909a58d3b4d7e4c19a36cf99a6
DaGhostman Dimitrov <daghostman.dimitrov@gmail.com>
2015-05-05 17:11:21 +0100

    Finished tests for Request object Added file for response tests

--- tests/Test/Http/RequestTest.php
+++ tests/Test/Http/RequestTest.php
@@ -8,6 +8,9 @@ class RequestTest extends \PHPUnit_Framework_TestCase
 {
     protected $server = [];
     protected $url;
+    /**
+     * @type \Wave\Framework\Interfaces\Http\RequestInterface
+     */
     protected $request;
 
     protected function setUp()
@@ -25,28 +28,20 @@ class RequestTest extends \PHPUnit_Framework_TestCase
             ->setMethods(['getMethod', 'getPath'])
             ->getMock();
 
-        $this->request = (new Request($this->server['method'], $this->url))
-            ->addHeaders($this->server);
+        $this->request = new Request($this->server['method'], $this->url, $this->server);
     }
 
-//    public function testImmutability()
-//    {
-//        $hash = $request = new Request('GET', $this->url);
-//        $request->addHeader('Test', '55');
-//        $this->assertSame($hash, $request);
-//
-//        //var_dump($r);
-//    }
-
-    public function testMethodExistence()
+    public function testBadHttpMethod()
     {
-        $this->assertTrue(method_exists($this->request, 'getHeader'));
+        $this->setExpectedException('\InvalidArgumentException');
+        new Request('READ', $this->url);
     }
 
     public function testHeaderGetters()
     {
         $this->assertSame('UnitTest', $this->request->getHeader('HTTP_USER_AGENT'));
         $this->assertSame('OK', $this->request->getHeader('x-legit-header'));
+        $this->assertNull($this->request->getHeader('non-existing-header'));
     }
 
     public function testHeadersExistence()
@@ -54,6 +49,69 @@ class RequestTest extends \PHPUnit_Framework_TestCase
         $this->assertNotSame($this->server, $this->request->getHeaders());
         $this->assertSame($this->server['HTTP_USER_AGENT'], $this->request->getHeader('HTTP_USER_AGENT'));
         $this->assertSame($this->server['x-legit-header'], $this->request->getHeader('x-legit-header'));
+    }
+
+    public function testConstructorHeaders()
+    {
+        $req = new Request('GET', $this->url, [
+            'User-Agent' => 'UnitTest',
+            'x-legit-header' => 'OK',
+            'content-type' => 'text/plain'
+        ]);
+
+        $this->assertTrue($req->hasHeader('USER-AGENT'));
+        $this->assertTrue($req->hasHeader('X-LEGIT-HEADER'));
+        $this->assertTrue($req->hasHeader('CONTENT-TYPE'));
+
+        $this->assertSame('UnitTest', $req->getHeader('user-agent'));
+        $this->assertSame('OK', $req->getHeader('x-legit-header'));
+        $this->assertSame('text/plain', $req->getHeader('content-type'));
+    }
+
+    public function testImmutability()
+    {
+        $hash = $request = new Request('GET', $this->url);
+        $request = $request->addHeader('Test', '55');
+        $this->assertNotSame($hash, $request);
+
+
+    }
+
+    public function testHeaderAppending()
+    {
+        $request = $this->request->addHeader('x-legit-header', 'PERFECT');
+        $this->assertSame(['OK', 'PERFECT'], $request->getHeader('x-legit-header'));
+    }
+    public function testSetterOverwriting()
+
+    {
+        $request = $this->request->addHeader('X-Legit-Header', 'PERFECT', false);
+        $this->assertSame('PERFECT', $request->getHeader('x-legit-header'));
+    }
+
+    public function testMultiHeaderAppending()
+    {
+        $request = new Request('get', $this->url, []);
+        $request = $request->addHeaders([
+            'x-legit-headers' => ['PERFECTLY', 'WORKING'],
+            'x-legit-header' => '!!!'
+        ]);
+
+        $this->assertEquals(
+            ['X-Legit-Header' =>
+                 ['!!!'],
+             'X-Legit-Headers' =>
+                 ['PERFECTLY', 'WORKING']
+            ],
+            $request->getHeaders()
+        );
+    }
+
+    public function testAdditionalGetters()
+    {
+        $this->assertEmpty($this->request->getBody());
+        $this->assertSame($this->url, $this->request->getUrl());
+        $this->assertSame('GET', $this->request->getMethod());
     }
 
     protected function tearDown()
---
+++ tests/Test/Http/ResponseTest.php
@@ -0,0 +1,7 @@
+<?php
+namespace Test\Http;
+
+class ResponseTest extends \PHPUnit_Framework_TestCase
+{
+
+}
