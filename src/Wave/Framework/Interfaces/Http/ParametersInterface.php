0fb51b82fba129629b1986ce08c6d91d6d2b462e
DaGhostman Dimitrov <daghostman.dimitrov@gmail.com>
2015-06-12 17:15:11 +0100

    Updated Parameter and Query interfaces

--- src/Wave/Framework/Interfaces/Http/ParametersInterface.php
+++ src/Wave/Framework/Interfaces/Http/ParametersInterface.php
@@ -5,9 +5,47 @@ namespace Wave\Framework\Interfaces\Http;
 
 interface ParametersInterface
 {
-    public function __construct(RequestInterface $request);
-    public function fetch($name);
-    public function has($name);
+    //public function __construct(RequestInterface $request);
+
+    /**
+     * Return a parameter with $name
+     *
+     * @param string $name
+     *
+     * @return mixed
+     */
+    public function get($name);
+
+    /**
+     * Add a new parameter
+     *
+     * @param string $name
+     * @param mixed $value
+     *
+     * @return mixed
+     */
     public function set($name, $value);
+
+    /**
+     * Check if a parameter exists
+     *
+     * @param string $name
+     *
+     * @return mixed
+     */
+    public function has($name);
+
+    /**
+     * Remove a parameter
+     *
+     * @param string $name
+     */
     public function remove($name);
+
+    /**
+     * Returns an array with all the parameters
+     *
+     * @return array
+     */
+    public function export();
 }
--- src/Wave/Framework/Interfaces/Http/QueryInterface.php
+++ src/Wave/Framework/Interfaces/Http/QueryInterface.php
@@ -8,48 +8,6 @@ namespace Wave\Framework\Interfaces\Http;
  */
 interface QueryInterface
 {
-    /**
-     * Allows constructor injection of the query string,
-     * or already parsed array with parameters.
-     *
-     * @param string|array $query
-     */
-    public function __construct($query);
-
-    /**
-     * Return a parameter with $name
-     *
-     * @param string $name
-     *
-     * @return mixed
-     */
-    public function get($name);
-
-    /**
-     * Add a new parameter
-     *
-     * @param string $name
-     * @param mixed $value
-     *
-     * @return mixed
-     */
-    public function set($name, $value);
-
-    /**
-     * Check if a parameter exists
-     *
-     * @param string $name
-     *
-     * @return mixed
-     */
-    public function has($name);
-
-    /**
-     * Remove a parameter
-     *
-     * @param string $name
-     */
-    public function remove($name);
 
     /**
      * Import a new set parameters. The new parameters should be
