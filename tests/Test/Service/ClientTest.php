992f46fc59eb98bc4787bbe5918add0619aeca15
DaGhostman <daghostman.dimitrov@gmail.com>
2015-07-12 04:04:56 +0100

    Added minimalistic HTTP Client based on curl

---
+++ tests/Test/Service/ClientTest.php
@@ -0,0 +1,77 @@
+<?php
+namespace Test\Service;
+
+use InterNations\Component\HttpMock\PHPUnit\HttpMockTrait;
+use Stub\MockUrl;
+use Stub\StubRequest;
+use Stub\StubResponse;
+use Wave\Framework\Service\Client;
+
+class ClientTest extends \PHPUnit_Framework_TestCase
+{
+    use HttpMockTrait;
+
+    private $client;
+
+    public static function setUpBeforeClass()
+    {
+        static::setUpHttpMockBeforeClass('8082', 'localhost');
+    }
+
+    public static function tearDownAfterClass()
+    {
+        static::tearDownHttpMockAfterClass();
+    }
+
+    public function setUp()
+    {
+        $this->setUpHttpMock();
+
+        $this->http->mock
+            ->when()
+            ->methodIs('GET')
+            ->pathIs('/api')
+            ->then()
+                ->statusCode(200)
+                ->header('User-Agent', 'PHPUnit/TestCase')
+                ->body('{}')
+            ->end();
+
+        $this->http->setUp();
+
+        $this->client = new Client(new StubRequest('GET', new MockUrl('/api', null, 'localhost', 8082)));
+    }
+
+    public function tearDown()
+    {
+        $this->tearDownHttpMock();
+    }
+
+    public function testSimpleRequest()
+    {
+        $this->assertSame(
+            '{}', $this->client->send(new StubResponse())->getBody()
+        );
+    }
+
+    public function testRequestWithHeaders()
+    {
+        $this->client->withHeaders();
+
+        $this->assertSame('PHPUnit/TestCase', $this->client->send(new StubResponse())->getHeader('User-Agent'));
+    }
+
+    public function testRequestWithoutHeaders()
+    {
+
+        $this->assertNull($this->client->send(new StubResponse())->getHeader('User-Agent'));
+    }
+
+    public function testCurlResource()
+    {
+        $this->assertTrue(is_resource(
+            (new Client(new StubRequest('GET', new MockUrl('/api', null, 'localhost', 8082))))
+                ->getCurl())
+        );
+    }
+}
