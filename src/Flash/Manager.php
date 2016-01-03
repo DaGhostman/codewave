b5226869f21c6fbc1e828ed5b2645830ba01f269
Dimitar Dimitrov <daghostman.dimitrov@gmail.com>
2016-01-03 02:00:06 +0000

    Flash\Manager now implements countable and provides a helper method to determine if there are any messages stored

--- src/Flash/Manager.php
+++ src/Flash/Manager.php
@@ -12,7 +12,7 @@ namespace Wave\Framework\Flash;
  * Class Manager
  * @package Wave\Framework\Flash
  */
-class Manager
+class Manager implements \Countable
 {
     const SUCCESS = 0;
     const INFO = 1;
@@ -116,5 +116,30 @@ class Manager
                 $_SESSION['_flash'][$type] = $messages;
             }
         }
+    }
+
+    /**
+     * Checks if the Message Queue is empty
+     *
+     * @return bool
+     */
+    public function isEmpty()
+    {
+        return count($this) > 0;
+    }
+
+    /**
+     * Count elements of an object
+     * @link http://php.net/manual/en/countable.count.php
+     * @return int The custom count as an integer.
+     * </p>
+     * <p>
+     * The return value is cast to an integer.
+     * @since 5.1.0
+     */
+    public function count()
+    {
+        $count = count($this->messages, true) - 5;
+        return  $count > 0 ? $count : 0;
     }
 }
